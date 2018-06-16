e78a2f58c42bcf19f48fdc99e442f4825bc8fd8a
OFFICE\yanisimov <yanisimov@gmail.com>
2018-06-15 17:32:17 -0700

    Adding method overload in order to support same metrics to multiple pushgateway endpoints - fault-tolerance

--- src/Prometheus.Client/MetricPush.cs
+++ src/Prometheus.Client/MetricPush.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,12 +10,30 @@ namespace Prometheus.Client
     public class MetricPush
     {
         public const string ContentType = "text/plain; version=0.0.4";
+
+        /// <summary>
+        /// Push metrics to single pushgateway endpoint
+        /// </summary>
+        /// <param name="endpoint">PushGateway endpoint</param>
+        /// <param name="job">job name</param>
+        /// <param name="instance">instance</param>
+        /// <param name="contentType">Content-Type</param>
+        /// <returns></returns>
         public async Task PushAsync(string endpoint, string job, string instance, string contentType = ContentType)
         {
-            if (string.IsNullOrEmpty(endpoint))
-            {
-                throw new ArgumentNullException(nameof(endpoint));
-            }
+            await PushAsync(new[] {endpoint}, job, instance, contentType);
+        }
+
+        /// <summary>
+        /// Push metrics to multiple pushgateway endpoints (fault-tolerance)
+        /// </summary>
+        /// <param name="endpoints">multiple pushgateway enpoints (fault-tolerance)</param>
+        /// <param name="job">job name</param>
+        /// <param name="instance">instance name</param>
+        /// <param name="contentType">content-type</param>
+        /// <returns></returns>
+        public async Task PushAsync(string[] endpoints, string job, string instance, string contentType = ContentType)
+        {
             if (string.IsNullOrEmpty(job))
             {
                 throw new ArgumentNullException(nameof(job));
@@ -27,19 +46,36 @@ namespace Prometheus.Client
             var streamContent = new StreamContent(memoryStream);
 
             var httpClient = new HttpClient();
-            var url = $"{endpoint.TrimEnd('/')}/job/{job}";
-            if (!string.IsNullOrEmpty(instance))
+            var tasks = new List<Task<HttpResponseMessage>>(endpoints.Length);
+
+            foreach (var endpoint in endpoints)
             {
-                url = $"{url}/instance/{instance}";
+                if (string.IsNullOrEmpty(endpoint))
+                {
+                    throw new ArgumentNullException(nameof(endpoint));
+                }
+
+                var url = $"{endpoint.TrimEnd('/')}/job/{job}";
+                if (!string.IsNullOrEmpty(instance))
+                {
+                    url = $"{url}/instance/{instance}";
+                }
+
+                if (!Uri.TryCreate(url, UriKind.Absolute, out var targetUrl))
+                {
+                    throw new ArgumentException("Endpoint must be a valid url", nameof(endpoint));
+                }
+
+                tasks.Add(httpClient.PostAsync(targetUrl, streamContent));
             }
 
-            if (!Uri.TryCreate(url, UriKind.Absolute, out var targetUrl))
-            {
-                throw new ArgumentException("Endpoint must be a valid url", nameof(endpoint));
-            }
+            await Task.WhenAll(tasks);
 
-            var response = await httpClient.PostAsync(targetUrl, streamContent);
-            response.EnsureSuccessStatusCode();
+            foreach (var task in tasks)
+            {
+                var response = await task;
+                response.EnsureSuccessStatusCode();
+            }
         }
     }
 }
