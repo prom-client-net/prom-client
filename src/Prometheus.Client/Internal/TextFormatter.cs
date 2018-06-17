f1573162d59c7fc4dc091e74c309f7b0c7a7124b
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2018-06-17 23:11:31 +0300

    TextFomatter: now Stream is leaveOpen

--- src/Prometheus.Client/Internal/TextFormatter.cs
+++ src/Prometheus.Client/Internal/TextFormatter.cs
@@ -17,7 +17,7 @@ namespace Prometheus.Client.Internal
         public static void Format(Stream destination, IEnumerable<CMetricFamily> metrics)
         {
             var metricFamilys = metrics.ToArray();
-            using (var streamWriter = new StreamWriter(destination, _encoding))
+            using (var streamWriter = new StreamWriter(destination, _encoding, 1024, true))
             {
                 streamWriter.NewLine = "\n";
                 foreach (var metricFamily in metricFamilys)
@@ -72,8 +72,11 @@ namespace Prometheus.Client.Internal
 
                 foreach (var quantileValuePair in cMetric.CSummary.Quantiles)
                 {
-                    var quantile = double.IsPositiveInfinity(quantileValuePair.Quantile) ? "+Inf" : quantileValuePair.Quantile.ToString(CultureInfo.InvariantCulture);
-                    streamWriter.WriteLine(SimpleValue(familyName, quantileValuePair.Value, cMetric.Labels.Concat(new[] {new CLabelPair {Name = "quantile", Value = quantile}})));
+                    var quantile = double.IsPositiveInfinity(quantileValuePair.Quantile)
+                        ? "+Inf"
+                        : quantileValuePair.Quantile.ToString(CultureInfo.InvariantCulture);
+                    streamWriter.WriteLine(SimpleValue(familyName, quantileValuePair.Value,
+                        cMetric.Labels.Concat(new[] { new CLabelPair { Name = "quantile", Value = quantile } })));
                 }
             }
             else if (cMetric.CHistogram != null)
@@ -83,7 +86,8 @@ namespace Prometheus.Client.Internal
                 foreach (var bucket in cMetric.CHistogram.Buckets)
                 {
                     var value = double.IsPositiveInfinity(bucket.UpperBound) ? "+Inf" : bucket.UpperBound.ToString(CultureInfo.InvariantCulture);
-                    streamWriter.WriteLine(SimpleValue(familyName, bucket.CumulativeCount, cMetric.Labels.Concat(new[] {new CLabelPair {Name = "le", Value = value}}), "_bucket"));
+                    streamWriter.WriteLine(SimpleValue(familyName, bucket.CumulativeCount,
+                        cMetric.Labels.Concat(new[] { new CLabelPair { Name = "le", Value = value } }), "_bucket"));
                 }
             }
         }
@@ -108,8 +112,11 @@ namespace Prometheus.Client.Internal
 
                 foreach (var quantileValuePair in cMetric.CSummary.Quantiles)
                 {
-                    var quantile = double.IsPositiveInfinity(quantileValuePair.Quantile) ? "+Inf" : quantileValuePair.Quantile.ToString(CultureInfo.InvariantCulture);
-                    s.AppendLine(SimpleValue(familyName, quantileValuePair.Value, cMetric.Labels.Concat(new[] {new CLabelPair {Name = "quantile", Value = quantile}})));
+                    var quantile = double.IsPositiveInfinity(quantileValuePair.Quantile)
+                        ? "+Inf"
+                        : quantileValuePair.Quantile.ToString(CultureInfo.InvariantCulture);
+                    s.AppendLine(SimpleValue(familyName, quantileValuePair.Value,
+                        cMetric.Labels.Concat(new[] { new CLabelPair { Name = "quantile", Value = quantile } })));
                 }
             }
             else if (cMetric.CHistogram != null)
@@ -119,7 +126,8 @@ namespace Prometheus.Client.Internal
                 foreach (var bucket in cMetric.CHistogram.Buckets)
                 {
                     var value = double.IsPositiveInfinity(bucket.UpperBound) ? "+Inf" : bucket.UpperBound.ToString(CultureInfo.InvariantCulture);
-                    s.AppendLine(SimpleValue(familyName, bucket.CumulativeCount, cMetric.Labels.Concat(new[] {new CLabelPair {Name = "le", Value = value}}), "_bucket"));
+                    s.AppendLine(SimpleValue(familyName, bucket.CumulativeCount, cMetric.Labels.Concat(new[] { new CLabelPair { Name = "le", Value = value } }),
+                        "_bucket"));
                 }
             }
 
