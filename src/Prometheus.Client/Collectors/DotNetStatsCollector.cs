d9e56aef49b5c069c1f0f6b60df3468211816493
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2018-09-09 00:58:19 +0300

    extract default 'Process' metrics to ProcessCollector

--- src/Prometheus.Client/Collectors/DotNetStatsCollector.cs
+++ src/Prometheus.Client/Collectors/DotNetStatsCollector.cs
@@ -13,16 +13,8 @@ namespace Prometheus.Client.Collectors
     {
         private readonly MetricFactory _metricFactory;
         private readonly List<Counter.ThisChild> _collectionCounts = new List<Counter.ThisChild>();
-        private readonly Process _process;
-        private Counter _cpuTotal;
-        private Counter _perfErrors;
         private Gauge _totalMemory;
-        
-        private Gauge _numThreads;
-        private Gauge _privateMemorySize;
-        private Gauge _virtualMemorySize;
-        private Gauge _workingSet;
-
+        private Counter _errorCounter;
 
         /// <inheritdoc />
         /// <summary>
@@ -39,43 +31,27 @@ namespace Prometheus.Client.Collectors
         public DotNetStatsCollector(MetricFactory metricFactory)
         {
             _metricFactory = metricFactory;
-            _process = Process.GetCurrentProcess();
         }
 
         /// <inheritdoc />
         public void RegisterMetrics()
         {
-            _perfErrors = _metricFactory.CreateCounter("dotnet_collection_errors_total", "Total number of errors that occured during collections");
+            _errorCounter = _metricFactory.CreateCounter("dotnet_collection_errors_total", "Total number of errors that occured during collections");
 
             var collectionCountsParent = _metricFactory.CreateCounter("dotnet_collection_count_total", "GC collection count", "generation");
-
-            for (var gen = 0; gen <= GC.MaxGeneration; gen++)
-                _collectionCounts.Add(collectionCountsParent.Labels(gen.ToString()));
-
-            _cpuTotal = _metricFactory.CreateCounter("process_cpu_seconds_total", "Total user and system CPU time spent in seconds");
 
             // .net specific metrics
             _totalMemory = _metricFactory.CreateGauge("dotnet_totalmemory", "Total known allocated memory");
 
-            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            _metricFactory.CreateGauge("process_start_time_seconds", "Start time of the process since unix epoch in seconds")
-                .Set((_process.StartTime.ToUniversalTime() - epoch).TotalSeconds);
-            
-            
-            _virtualMemorySize = _metricFactory.CreateGauge("process_virtual_bytes", "Process virtual memory size");
-            _workingSet = _metricFactory.CreateGauge("process_working_set", "Process working set");
-            _privateMemorySize = _metricFactory.CreateGauge("process_private_bytes", "Process private memory size");
-            _numThreads = _metricFactory.CreateGauge("process_num_threads", "Total number of threads");
-            
-            _metricFactory.CreateGauge("process_processid", "Process ID").Set(_process.Id); // todo: Untyped
+
+            for (var gen = 0; gen <= GC.MaxGeneration; gen++)
+                _collectionCounts.Add(collectionCountsParent.Labels(gen.ToString()));
         }
 
         public void UpdateMetrics()
         {
             try
             {
-                _process.Refresh();
-
                 for (var gen = 0; gen <= GC.MaxGeneration; gen++)
                 {
                     var collectionCount = _collectionCounts[gen];
@@ -83,16 +59,10 @@ namespace Prometheus.Client.Collectors
                 }
 
                 _totalMemory.Set(GC.GetTotalMemory(false));
-                _cpuTotal.Inc(_process.TotalProcessorTime.TotalSeconds - _cpuTotal.Value);
-                
-                _virtualMemorySize.Set(_process.VirtualMemorySize64);
-                _workingSet.Set(_process.WorkingSet64);
-                _privateMemorySize.Set(_process.PrivateMemorySize64);
-                _numThreads.Set(_process.Threads.Count);
             }
             catch (Exception)
             {
-                _perfErrors.Inc();
+                _errorCounter.Inc();
             }
         }
     }
