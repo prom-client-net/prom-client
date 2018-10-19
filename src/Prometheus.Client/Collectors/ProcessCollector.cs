d1d66e5d6632e8471cabd84ccdc210be0e50f76f
Sergey Zhizhenko <zlobnyiserg@gmail.com>
2018-10-19 18:04:21 +0300

    Revert "Added dot to embedded metric's help (to correspond same embedded Pushgateway metrics)"
    This reverts commit ec4649c3b799a8c62d958dcee0a270477496802a.

--- src/Prometheus.Client/Collectors/ProcessCollector.cs
+++ src/Prometheus.Client/Collectors/ProcessCollector.cs
@@ -42,19 +42,19 @@ namespace Prometheus.Client.Collectors
         /// <inheritdoc />
         public void RegisterMetrics()
         {
-            _errorCounter = _metricFactory.CreateCounter("process_errors_total", "Total number of errors that occured during collections.");
+            _errorCounter = _metricFactory.CreateCounter("process_errors_total", "Total number of errors that occured during collections");
 
-            _cpuTotal = _metricFactory.CreateCounter("process_cpu_seconds_total", "Total user and system CPU time spent in seconds.");
+            _cpuTotal = _metricFactory.CreateCounter("process_cpu_seconds_total", "Total user and system CPU time spent in seconds");
 
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             
-            _metricFactory.CreateGauge("process_start_time_seconds", "Start time of the process since unix epoch in seconds.")
+            _metricFactory.CreateGauge("process_start_time_seconds", "Start time of the process since unix epoch in seconds")
                 .Set((_process.StartTime.ToUniversalTime() - epoch).TotalSeconds);
             
-                        _virtualMemorySize = _metricFactory.CreateGauge("process_virtual_bytes", "Process virtual memory size.");
-            _workingSet = _metricFactory.CreateGauge("process_working_set", "Process working set.");
-            _privateMemorySize = _metricFactory.CreateGauge("process_private_bytes", "Process private memory size.");
-            _numThreads = _metricFactory.CreateGauge("process_num_threads", "Total number of threads.");
+                        _virtualMemorySize = _metricFactory.CreateGauge("process_virtual_bytes", "Process virtual memory size");
+            _workingSet = _metricFactory.CreateGauge("process_working_set", "Process working set");
+            _privateMemorySize = _metricFactory.CreateGauge("process_private_bytes", "Process private memory size");
+            _numThreads = _metricFactory.CreateGauge("process_num_threads", "Total number of threads");
             
             _metricFactory.CreateGauge("process_processid", "Process ID").Set(_process.Id); // todo: Untyped           
         }
