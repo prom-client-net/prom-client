dfc359888d078394b5b90fb3991d9c928c9035d3
sanych-sun <alexes.polyakov@gmail.com>
2019-03-28 01:36:41 -0700

    Remove minor allocation while scrapping

--- src/Prometheus.Client/Collectors/DotNetStats/GCCollectionCountCollector.cs
+++ src/Prometheus.Client/Collectors/DotNetStats/GCCollectionCountCollector.cs
@@ -9,11 +9,17 @@ namespace Prometheus.Client.Collectors.DotNetStats
     {
         private const string _help = "GC collection count";
         private const string _name = "dotnet_collection_count_total";
+        private readonly string[] _genNames;
 
         public GCCollectionCountCollector()
         {
             Configuration = new CollectorConfiguration(nameof(GCCollectionCountCollector));
             MetricNames = new[] { _name };
+            _genNames = new string[GC.MaxGeneration + 1];
+            for (var gen = 0; gen <= GC.MaxGeneration; gen++)
+            {
+                _genNames[gen] = gen.ToString();
+            }
         }
 
         public ICollectorConfiguration Configuration { get; }
@@ -28,7 +34,7 @@ namespace Prometheus.Client.Collectors.DotNetStats
             {
                 writer.StartSample()
                     .StartLabels()
-                    .WriteLabel("generation", gen.ToString())
+                    .WriteLabel("generation", _genNames[gen])
                     .EndLabels()
                     .WriteValue(GC.CollectionCount(gen))
                     .EndSample();
