cc697a3cf049d2e6ba3da71463f5d88535d3d943
Sergey Kuznetsov <cmt@phnx47.net>
2019-10-15 00:09:50 +0300

    Remove codecov_token

--- test.sh
+++ test.sh
@@ -4,6 +4,5 @@ dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
 export CI='True'
 export APPVEYOR='True'
-export CODECOV_TOKEN=${codecov_token}
 
 bash <(curl -s https://codecov.io/bash) || echo 'Codecov failed to upload'
