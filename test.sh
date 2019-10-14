94ea543702716abbaa97a0d903892bacffeee99d
Sergey Kuznetsov <cmt@phnx47.net>
2019-10-14 23:46:34 +0300

    Codecov test

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
  
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput='../../coverage.xml'
 
-bash <(curl -s https://codecov.io/bash) -f "coverage.xml" -t ${codecov_token}
+export CODECOV_TOKEN=${codecov_token}
+
+bash <(curl -s https://codecov.io/bash) -f 'coverage.xml' || echo 'Codecov failed to upload'
