71c4552c829e962ad964a91f7c988883ccee34a9
Sergey Kuznetsov <cmt@phnx47.net>
2019-10-14 22:50:39 +0300

    Codecov try fix

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
  
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput='../../coverage.xml'
+echo '1'
+echo ${codecov_token}
+echo '2'
+echo ${env:codecov_token}
 bash <(curl -s https://codecov.io/bash) -f "coverage.xml" -t ${codecov_token}
