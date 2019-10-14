b1f0fc761e3e460ce4fae2a47a19a6ed00c8a18b
Sergey Kuznetsov <cmt@phnx47.net>
2019-10-14 23:52:42 +0300

    Codecov setup AppVeyor

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
  
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput='../../coverage.xml'
 
+export CI="True"
+export APPVEYOR="True"
 export CODECOV_TOKEN=${codecov_token}
 
 bash <(curl -s https://codecov.io/bash) -f 'coverage.xml' || echo 'Codecov failed to upload'
