9363570142f0ffc713da604286c925c8341e4147
Serge K <cmt@phnx47.net>
2020-07-29 01:16:49 +0700

    Remove appveyor

--- pack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ${APPVEYOR_REPO_TAG} == "true"  ; then
-  echo "Pack for NuGet"
-  dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client -c Release --include-symbols --include-source --no-build -o artifacts/nuget
-else
-  echo "Pack for MyGet"
-  dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client -c Release --include-symbols --include-source --no-build --version-suffix build${APPVEYOR_BUILD_NUMBER} -o artifacts/myget
-fi
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-
-export CI='True'
-export APPVEYOR='True'
-
-bash <(curl -s https://codecov.io/bash) || echo 'Codecov failed to upload'
