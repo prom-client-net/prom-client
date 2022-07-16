4dc6ced98627486015f6d9a6873ff04d3556338c
Serge K <cmt@phnx47.net>
2022-07-16 17:18:16 +0700

    feat: add addLegacyMetricNames to UseDotNetStats()

--- src/Prometheus.Client/Collectors/DotNetStats/CollectorRegistryExtensions.cs
+++ src/Prometheus.Client/Collectors/DotNetStats/CollectorRegistryExtensions.cs
@@ -2,15 +2,15 @@ namespace Prometheus.Client.Collectors.DotNetStats
 {
     public static class CollectorRegistryExtensions
     {
-        public static ICollectorRegistry UseDotNetStats(this ICollectorRegistry registry)
+        public static ICollectorRegistry UseDotNetStats(this ICollectorRegistry registry, bool addLegacyMetricNames = false)
         {
-            return UseDotNetStats(registry, "");
+            return UseDotNetStats(registry, string.Empty, addLegacyMetricNames);
         }
 
-        public static ICollectorRegistry UseDotNetStats(this ICollectorRegistry registry, string prefixName)
+        public static ICollectorRegistry UseDotNetStats(this ICollectorRegistry registry, string prefixName, bool addLegacyMetricNames = false)
         {
             registry.Add(new GCCollectionCountCollector(prefixName));
-            registry.Add(new GCTotalMemoryCollector(prefixName));
+            registry.Add(new GCTotalMemoryCollector(prefixName, addLegacyMetricNames));
 
             return registry;
         }
