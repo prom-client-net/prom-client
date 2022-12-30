c1650ad88af154f0d9a28b62dcc1d85bbbac947f
Serge K <cmt@phnx47.net>
2022-12-30 21:29:25 +0700

    test(histogram): cleanup factory tests

--- tests/Prometheus.Client.Tests/HistogramTests/FactoryTests.cs
+++ tests/Prometheus.Client.Tests/HistogramTests/FactoryTests.cs
@@ -2,152 +2,122 @@ using System;
 using Prometheus.Client.Collectors;
 using Xunit;
 
-namespace Prometheus.Client.Tests.HistogramTests
+namespace Prometheus.Client.Tests.HistogramTests;
+
+public class FactoryTests
 {
-    public class FactoryTests
+    private readonly IMetricFactory _factory;
+
+    public FactoryTests()
     {
-        [Fact]
-        public void ThrowOnNameConflict_Strings()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        _factory = new MetricFactory(new CollectorRegistry());
+    }
 
-            factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
+    [Fact]
+    public void ThrowOnNameConflict_Strings()
+    {
+        _factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
 
-            Assert.Throws<InvalidOperationException>(() => factory.CreateHistogram("test_histogram", string.Empty, "label1", "testlabel"));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateHistogram("test_histogram", string.Empty, new[] { "label1" }));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2", "label3"));
-        }
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateHistogram("test_histogram", string.Empty, "label1", "testlabel"));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateHistogram("test_histogram", string.Empty, new[] { "label1" }));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2", "label3"));
+    }
 
-        [Fact]
-        public void ThrowOnNameConflict_Tuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+    [Fact]
+    public void ThrowOnNameConflict_Tuple()
+    {
+        _factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
 
-            factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create("label1")));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateHistogram("test_histogram", string.Empty, ("label1", "testlabel")));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2", "label3")));
+    }
 
-            Assert.Throws<InvalidOperationException>(() => factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create("label1")));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateHistogram("test_histogram", string.Empty, ("label1", "testlabel")));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2", "label3")));
-        }
+    [Fact]
+    public void SameLabelsProducesSameMetric_Strings()
+    {
+        var metric1 = _factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
+        var metric2 = _factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_Strings()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        Assert.Equal(metric1, metric2);
+    }
 
-            var counter1 = factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
-            var counter2 = factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
+    [Fact]
+    public void SameLabelsProducesSameMetric_Tuples()
+    {
+        var metric1 = _factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
+        var metric2 = _factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
 
-            Assert.Equal(counter1, counter2);
-        }
+        Assert.Equal(metric1, metric2);
+    }
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_Tuples()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+    [Fact]
+    public void SameLabelsProducesSameMetric_StringAndTuple()
+    {
+        var metric1 = _factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
+        var metric2 = _factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
 
-            var counter1 = factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
-            var counter2 = factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
+        // Cannot compare metrics families, because of different contracts, should check if sample the same
+        Assert.Equal(metric1.Unlabelled, metric2.Unlabelled);
+    }
 
-            Assert.Equal(counter1, counter2);
-        }
+    [Fact]
+    public void SameLabelsProducesSameMetric_Empty()
+    {
+        var metric1 = _factory.CreateHistogram("test_histogram", string.Empty);
+        var metric2 = _factory.CreateHistogram("test_histogram", string.Empty);
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_StringAndTuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        Assert.Equal(metric1, metric2);
+    }
 
-            var counter1 = factory.CreateHistogram("test_histogram", string.Empty, "label1", "label2");
-            var counter2 = factory.CreateHistogram("test_histogram", string.Empty, ("label1", "label2"));
+    [Fact]
+    public void SameLabelsProducesSameMetric_EmptyStrings()
+    {
+        var metric1 = _factory.CreateHistogram("test_histogram", string.Empty, Array.Empty<string>());
+        var metric2 = _factory.CreateHistogram("test_histogram", string.Empty, Array.Empty<string>());
 
-            // Cannot compare metrics families, because of different contracts, should check if sample the same
-            Assert.Equal(counter1.Unlabelled, counter2.Unlabelled);
-        }
+        Assert.Equal(metric1, metric2);
+    }
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_Empty()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+    [Fact]
+    public void SameLabelsProducesSameMetric_EmptyTuples()
+    {
+        var metric1 = _factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create());
+        var metric2 = _factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create());
 
-            var counter1 = factory.CreateHistogram("test_histogram", string.Empty);
-            var counter2 = factory.CreateHistogram("test_histogram", string.Empty);
+        Assert.Equal(metric1, metric2);
+    }
 
-            Assert.Equal(counter1, counter2);
-        }
+    [Fact]
+    public void SameLabelsProducesSameMetric_EmptyStringAndTuple()
+    {
+        var metric1 = _factory.CreateHistogram("test_histogram", string.Empty, Array.Empty<string>());
+        var metric2 = _factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create());
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_EmptyStrings()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        // Cannot compare metrics families, because of different contracts, should check if sample the same
+        Assert.Equal(metric1.Unlabelled, metric2.Unlabelled);
+    }
 
-            var counter1 = factory.CreateHistogram("test_histogram", string.Empty, Array.Empty<string>());
-            var counter2 = factory.CreateHistogram("test_histogram", string.Empty, Array.Empty<string>());
+    [Theory]
+    [InlineData("le")]
+    [InlineData("le", "label")]
+    [InlineData("label", "le")]
+    public void ThrowOnReservedLabelNames_Strings(params string[] labels)
+    {
+        Assert.Throws<ArgumentException>(() => _factory.CreateHistogram("test_Histogram", string.Empty, labels));
+    }
 
-            Assert.Equal(counter1, counter2);
-        }
+    [Fact]
+    public void ThrowOnReservedLabelNames_Tuple()
+    {
+        Assert.Throws<ArgumentException>(() => _factory.CreateHistogram("test_Histogram", string.Empty, ValueTuple.Create("le")));
+        Assert.Throws<ArgumentException>(() => _factory.CreateHistogram("test_Histogram", string.Empty, ("le", "label")));
+    }
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_EmptyTuples()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            var counter1 = factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create());
-            var counter2 = factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create());
-
-            Assert.Equal(counter1, counter2);
-        }
-
-        [Fact]
-        public void SameLabelsProducesSameMetric_EmptyStringAndTuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            var counter1 = factory.CreateHistogram("test_histogram", string.Empty, Array.Empty<string>());
-            var counter2 = factory.CreateHistogram("test_histogram", string.Empty, ValueTuple.Create());
-
-            // Cannot compare metrics families, because of different contracts, should check if sample the same
-            Assert.Equal(counter1.Unlabelled, counter2.Unlabelled);
-        }
-
-        [Theory]
-        [InlineData("le")]
-        [InlineData("le", "label")]
-        [InlineData("label", "le")]
-        public void ThrowOnReservedLabelNames_Strings(params string[] labels)
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            Assert.Throws<ArgumentException>(() => factory.CreateHistogram("test_Histogram", string.Empty, labels));
-        }
-
-        [Fact]
-        public void ThrowOnReservedLabelNames_Tuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            Assert.Throws<ArgumentException>(() => factory.CreateHistogram("test_Histogram", string.Empty, ValueTuple.Create("le")));
-            Assert.Throws<ArgumentException>(() => factory.CreateHistogram("test_Histogram", string.Empty, ("le", "label")));
-        }
-
-        [Fact]
-        public void SingleLabel_ConvertToTuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            var gauge = factory.CreateHistogram("metricname", "help", "label");
-            Assert.Equal(typeof(ValueTuple<string>), gauge.LabelNames.GetType());
-        }
+    [Fact]
+    public void SingleLabel_ConvertToTuple()
+    {
+        var metric = _factory.CreateHistogram("metricname", "help", "label");
+        Assert.Equal(typeof(ValueTuple<string>), metric.LabelNames.GetType());
     }
 }
