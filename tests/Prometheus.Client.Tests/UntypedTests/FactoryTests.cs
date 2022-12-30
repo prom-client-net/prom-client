930ab5193735cac96f12d2be643a7190f3d40d45
Serge K <cmt@phnx47.net>
2022-12-30 21:22:01 +0700

    test(untyped): cleanup factory tests

--- tests/Prometheus.Client.Tests/UntypedTests/FactoryTests.cs
+++ tests/Prometheus.Client.Tests/UntypedTests/FactoryTests.cs
@@ -2,141 +2,114 @@ using System;
 using Prometheus.Client.Collectors;
 using Xunit;
 
-namespace Prometheus.Client.Tests.UntypedTests
+namespace Prometheus.Client.Tests.UntypedTests;
+
+public class FactoryTests
 {
-    public class FactoryTests
+    private readonly IMetricFactory _factory;
+
+    public FactoryTests()
     {
-        [Fact]
-        public void ThrowOnNameConflict_Strings()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        _factory = new MetricFactory(new CollectorRegistry());
+    }
 
-            factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
+    [Fact]
+    public void ThrowOnNameConflict_Strings()
+    {
+        _factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
 
-            Assert.Throws<InvalidOperationException>(() => factory.CreateUntyped("test_untyped", string.Empty, "label1", "testlabel"));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateUntyped("test_untyped", string.Empty, new[] { "label1" }));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2", "label3"));
-        }
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateUntyped("test_untyped", string.Empty, "label1", "testlabel"));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateUntyped("test_untyped", string.Empty, new[] { "label1" }));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2", "label3"));
+    }
 
-        [Fact]
-        public void ThrowOnNameConflict_Tuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+    [Fact]
+    public void ThrowOnNameConflict_Tuple()
+    {
+        _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
 
-            factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create("label1")));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "testlabel")));
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2", "label3")));
+    }
 
-            Assert.Throws<InvalidOperationException>(() => factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create("label1")));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateUntyped("test_untyped", string.Empty, ("label1", "testlabel")));
-            Assert.Throws<InvalidOperationException>(() => factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2", "label3")));
-        }
+    [Fact]
+    public void ThrowOnNameConflict_StringAndTuple()
+    {
+        _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
 
-        [Fact]
-        public void ThrowOnNameConflict_StringAndTuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        Assert.Throws<InvalidOperationException>(() => _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "testlabel")));
+    }
 
-            factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
+    [Fact]
+    public void SameLabelsProducesSameMetric_Strings()
+    {
+        var metric1 = _factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
+        var metric2 = _factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
 
-            Assert.Throws<InvalidOperationException>(() => factory.CreateUntyped("test_untyped", string.Empty, ("label1", "testlabel")));
-        }
+        Assert.Equal(metric1, metric2);
+    }
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_Strings()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+    [Fact]
+    public void SameLabelsProducesSameMetric_Tuples()
+    {
+        var metric1 = _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
+        var metric2 = _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
 
-            var untyped1 = factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
-            var counter2 = factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
+        Assert.Equal(metric1, metric2);
+    }
 
-            Assert.Equal(untyped1, counter2);
-        }
+    [Fact]
+    public void SameLabelsProducesSameMetric_StringAndTuple()
+    {
+        var metric1 = _factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
+        var metric2 = _factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_Tuples()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        // Cannot compare metrics families, because of different contracts, should check if sample the same
+        Assert.Equal(metric1.Unlabelled, metric2.Unlabelled);
+    }
 
-            var untyped1 = factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
-            var counter2 = factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
+    [Fact]
+    public void SameLabelsProducesSameMetric_Empty()
+    {
+        var metric1 = _factory.CreateUntyped("test_untyped", string.Empty);
+        var metric2 = _factory.CreateUntyped("test_untyped", string.Empty);
 
-            Assert.Equal(untyped1, counter2);
-        }
+        Assert.Equal(metric1, metric2);
+    }
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_StringAndTuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+    [Fact]
+    public void SameLabelsProducesSameMetric_EmptyStrings()
+    {
+        var metric1 = _factory.CreateUntyped("test_untyped", string.Empty, Array.Empty<string>());
+        var metric2 = _factory.CreateUntyped("test_untyped", string.Empty, Array.Empty<string>());
 
-            var untyped1 = factory.CreateUntyped("test_untyped", string.Empty, "label1", "label2");
-            var counter2 = factory.CreateUntyped("test_untyped", string.Empty, ("label1", "label2"));
+        Assert.Equal(metric1, metric2);
+    }
 
-            // Cannot compare metrics families, because of different contracts, should check if sample the same
-            Assert.Equal(untyped1.Unlabelled, counter2.Unlabelled);
-        }
+    [Fact]
+    public void SameLabelsProducesSameMetric_EmptyTuples()
+    {
+        var metric1 = _factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create());
+        var metric2 = _factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create());
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_Empty()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
+        Assert.Equal(metric1, metric2);
+    }
 
-            var untyped1 = factory.CreateUntyped("test_untyped", string.Empty);
-            var counter2 = factory.CreateUntyped("test_untyped", string.Empty);
+    [Fact]
+    public void SameLabelsProducesSameMetric_EmptyStringAndTuple()
+    {
+        var metric1 = _factory.CreateUntyped("test_untyped", string.Empty, Array.Empty<string>());
+        var metric2 = _factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create());
 
-            Assert.Equal(untyped1, counter2);
-        }
+        // Cannot compare metrics families, because of different contracts, should check if sample the same
+        Assert.Equal(metric1.Unlabelled, metric2.Unlabelled);
+    }
 
-        [Fact]
-        public void SameLabelsProducesSameMetric_EmptyStrings()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            var untyped1 = factory.CreateUntyped("test_untyped", string.Empty, Array.Empty<string>());
-            var counter2 = factory.CreateUntyped("test_untyped", string.Empty, Array.Empty<string>());
-
-            Assert.Equal(untyped1, counter2);
-        }
-
-        [Fact]
-        public void SameLabelsProducesSameMetric_EmptyTuples()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            var untyped1 = factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create());
-            var counter2 = factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create());
-
-            Assert.Equal(untyped1, counter2);
-        }
-
-        [Fact]
-        public void SameLabelsProducesSameMetric_EmptyStringAndTuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            var untyped1 = factory.CreateUntyped("test_untyped", string.Empty, Array.Empty<string>());
-            var counter2 = factory.CreateUntyped("test_untyped", string.Empty, ValueTuple.Create());
-
-            // Cannot compare metrics families, because of different contracts, should check if sample the same
-            Assert.Equal(untyped1.Unlabelled, counter2.Unlabelled);
-        }
-
-        [Fact]
-        public void SingleLabel_ConvertToTuple()
-        {
-            var registry = new CollectorRegistry();
-            var factory = new MetricFactory(registry);
-
-            var gauge = factory.CreateUntyped("metricname", "help", "label");
-            Assert.Equal(typeof(ValueTuple<string>), gauge.LabelNames.GetType());
-        }
+    [Fact]
+    public void SingleLabel_ConvertToTuple()
+    {
+        var metric = _factory.CreateUntyped("metricname", "help", "label");
+        Assert.Equal(typeof(ValueTuple<string>), metric.LabelNames.GetType());
     }
 }
